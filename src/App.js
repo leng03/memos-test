7d608ddbf6b5b7260d3ef2a24efd71f18fc7fa5e
mfortenberry <mfortenberry@yorksolutions.net>
2022-04-22 14:10:52 -0500

    useState to keep track of a count

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
+import {useState} from "react";
 
+// All 'Functional React Components' are render functions
+// This function is called every time we want to render our
+//    application
+// Each FRC must return a single tag/element
 function App() {
-  return <h1>This is the main App</h1>
+    // useState returns an array with 2 elements
+    // The first element is the current value
+    // The second element is a function that we can call
+    //    to update the value
+    const [count, setCount] = useState(0)
+
+    function handleClick() {
+        console.log('clicked')
+        setCount(count + 1)
+    }
+
+    return <>
+        <h1>This is the main App</h1>
+        <h1>The count is {count}</h1>
+        <button onClick={handleClick}>Hello</button>
+    </>
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Up until now, we have done manual testing
+// I called it "no excuses" testing
+// Now we will use a framework
+// framework is called jest
+// We are no longer testing JS, we are testing
+//    ecma script
+// Ecma Script is a combination of JS and JSX
+
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/This is the main App/i);
   expect(linkElement).toBeInTheDocument();
 });
