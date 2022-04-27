c782964fef4585b9bcd541586d66dc6a62c6904a
leng03 <lengxiong03@gmail.com>
2022-04-27 14:43:05 -0500

    push

--- src/components/EditMemo.js
+++ src/components/EditMemo.js
@@ -1,11 +1,32 @@
 // responsible for editing a memo
 
-export default function EditMemo({memo}){
+import {useState} from "react";
+
+export default function EditMemo({memo, onCancel, onApply}){
     const {title, description, date, complete} = memo
+    const [newTitle, setNewTitle] = useState(title)
+    const [newDescription, setNewDescription] = useState(description)
+    const [newDate, setNewDate] = useState(date)
+    const [newComplete, setNewComplete] = useState(complete)
+
+    function handleApply(){
+        onApply({
+            title: newTitle,
+            description: newDescription,
+            date: new Date(newDate),
+            complete: newComplete
+        })
+    }
+
     return<>
-        <input defaultValue={title}/>
-        <input defaultValue={description}/>
-        <input type= "date" defaultValue={date.toISOString().substr(0,10)}/>
-        <input type= "checkbox" defaultChecked={complete}/>
+        <input defaultValue={title} onChange={event => setNewTitle(event.target.value)}/>
+        <input defaultValue={description}
+               onChange={event => setNewDescription(event.target.value)}/>
+        <input type= "date" defaultValue={date.toISOString().substr(0,10)}
+               onChange={event => setNewDate(event.target.value)}/>
+        <input type= "checkbox" defaultChecked={complete}
+               onChange={event => setNewComplete(event.target.checked)}/>
+        <button onClick={handleApply}>Apply</button>
+        <button onClick={onCancel}>Cancel</button>
     </>
 }
--- src/components/EditMemo.test.js
+++ src/components/EditMemo.test.js
@@ -1,5 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import EditMemo from "./EditMemo";
+import userEvent from "@testing-library/user-event";
+import {act} from "react-dom/test-utils";
 
 test("should show input fields with current memo data and false complete", () => {
     const memo = {
@@ -36,3 +38,82 @@ test("should show input fields with current memo data and true complete", () => 
     const completeElement = screen.getByRole("checkbox")
     expect(completeElement.checked).toBe(memo.complete)
 })
+
+it('should show a cancel and apply button', function () {
+    const  memo = {
+        title: "Title1",
+        date: new Date(),
+        description: "Desc1",
+        complete: true
+    }
+    render(<EditMemo memo={memo}/>)
+    expect(screen.getByText("Apply")).toBeInTheDocument()
+    expect(screen.getByText("Cancel")).toBeInTheDocument()
+});
+
+// Establish goal first (user perspective, draw interface, etc..)
+// What are the inputs and outputs to the function in order to achieve the goal
+//      aka the public interface
+// Think about how a human would test
+// translate to an automated form
+
+// TDD: Test driven development
+// TDD: Test driven design
+
+it('should call the onCancel fn when the cancel button is clicked', function () {
+    const  memo = {
+        title: "Title1",
+        date: new Date(),
+        description: "Desc1",
+        complete: true
+    }
+
+    const mock = jest.fn()
+    render(<EditMemo memo={memo} onCancel={mock}/>)
+    const cancelButton = screen.getByText("Cancel").click()
+    expect(mock).toHaveBeenCalled()
+
+});
+
+it('should call the onApply fn when the apply button is clicked', function () {
+    const  memo = {
+        title: "Title1",
+        date: new Date(),
+        description: "Desc1",
+        complete: true
+    }
+
+    const mock = jest.fn()
+    render(<EditMemo memo={memo} onApply={mock}/>)
+
+    const yesterday = new Date()
+    yesterday.setDate(yesterday.getDate()-1)
+    yesterday.setUTCHours(0)
+    yesterday.setMinutes(0)
+    yesterday.setSeconds(0)
+    yesterday.setMilliseconds(0)
+
+    const  expected = {
+        title: "new title",
+        date: yesterday,
+        description: "new desc",
+        complete: false
+    }
+    // getbydisplayvalue because they are input feild
+    const titleInput = screen.getByDisplayValue(memo.title)
+    const descInput = screen.getByDisplayValue(memo.description)
+    const dateInput = screen.getByDisplayValue(memo.date.toISOString().substr(0,10))
+
+    act(()=>{
+        userEvent.clear(titleInput)
+        userEvent.type(titleInput, expected.title)
+        userEvent.clear(descInput)
+        userEvent.type(descInput, expected.description)
+        userEvent.clear(dateInput)
+        userEvent.type(dateInput, expected.date.toISOString().substr(0, 10))
+        screen.getByRole("checkbox").click()
+    })
+
+    screen.getByText("Apply").click()
+    expect(mock).toHaveBeenCalledWith(expected)
+});
